12d7f43e68a04197baa15747f0a35a4b58147793
Jan Turon <janturon@email.cz>
2019-10-11 21:42:03 +0200

    task01 created

---
+++ task01.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+const char* toUpper(const char* input) {
+	// TO DO
+}
+
+int main() {
+	const char* input = "Hello, World!";
+	puts(toUpper(input)); // should print HELLO, WORLD!
+}
